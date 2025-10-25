80ac226e9a5c62ce994f7b0348b49c665be6095e
ssherliann <popova_anna@knu.ua>
2025-10-25 13:40:25 +0300

    update

---
+++ Models/ProfileViewModel.cs
@@ -0,0 +1,10 @@
+namespace WorkingSpaces.Models
+{
+    public class ProfileViewModel
+    {
+        public string Username { get; set; } = "";
+        public string FullName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string PhoneNumber { get; set; } = "";
+    }
+}
